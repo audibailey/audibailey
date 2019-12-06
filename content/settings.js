25b866db3dd08d5065cb5f2045dd4c74ddcd4e80
Audi Bailey <audibailey7@gmail.com>
2019-12-06 15:51:35 +1000

    Attempt to get forestry manage site stuff

---
+++ content/settings.js
@@ -0,0 +1,9 @@
+export default
+{
+    "title": "Hi! I'm Audi Bailey",
+    "subtitle": "Electrical Engineering and Computer Science Student",
+    "github": "https://www.github.com/audibailey",
+    "linkedin": "https://www.linkedin.com/in/audi-bailey",
+    "twitter": "/",
+    "footer": "Made with ❤️ by Audi Bailey"
+}
